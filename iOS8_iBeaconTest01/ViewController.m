b94d7ccbe303d20db6966786083fc5274737102e
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-12-11 15:26:40 +0900

    1st commit

--- iOS8_iBeaconTest01/ViewController.m
+++ iOS8_iBeaconTest01/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -17,11 +20,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _locationManager = [[CLLocationManager alloc]init];
+    _locationManager.delegate = self;
+    
+    [_locationManager requestAlwaysAuthorization]; // iOS8
+    
+    [self chkBeaconStart];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//
+// ビーコン監視を開始
+//
+-(void) chkBeaconStart{
+    
+    NSString *uuid = @"C768FFC6-B7C4-4079-8EC7-9094AAA19782";
+    CLBeaconRegion *region = [[CLBeaconRegion alloc]initWithProximityUUID:[[NSUUID alloc]initWithUUIDString:uuid] identifier:@"MyBeacon"];
+    
+    [_locationManager startMonitoringForRegion:region]; // ビーコン領域の監視
+    
+}
+
+//
+// 領域内に入った場合
+//
+- (void) locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    NSLog(@"didEnterRegion");
+}
+
+//
+// 領域外に出た場合
+//
+- (void) locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
+    NSLog(@"didExitRegion");
+}
+
+//
+// 監視中にエラーが発生した場合
+//
+- (void) locationManager:(CLLocationManager *)manager monitoringDidFailForRegion:(CLRegion *)region withError:(NSError *)error {
+    NSLog(@"monitoringDidFailForRegion");
 }
 
 @end
